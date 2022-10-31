8b87b5870fe11b19046a7aef5e29d361b29ee032
Miguel Tamargo <89949641+miguelangeltamargo@users.noreply.github.com>
2022-10-30 22:36:27 -0400

    Sprint 4 Commit

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,36 +1,96 @@
-const Post = require("../models/Post");
+const Author = require('../models/Author');
+const Books = require('../models/Books');
 
-exports.getAllPosts = async (req, res, next) => {
-  try {
-    const [posts, _] = await Post.findAll();
+exports.findAllAuthors = async (req, res, next) => {
+	try {
+		const [author, _] = await Author.findAllAuthors();
 
-    res.status(200).json({ count: posts.length, posts });
-  } catch (error) {
-    next(error);
-  }
+		res.status(200).json({ count: author.length, author});
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
 };
 
-exports.createNewPost = async (req, res, next) => {
-  try {
-    let { title, body } = req.body;
-    let post = new Post(title, body);
+exports.createNewAuthor = async (req, res, next) => {
+	try {
+		let {first_name, last_name, biography, publisher} = req.body;
+		let author = new Author(first_name, last_name, biography, publisher);
+		author = await author.save();
+		console.log(author);
 
-    post = await post.save();
-
-    res.status(201).json({ message: "Post created" });
-  } catch (error) {
-    next(error);
-  }
+		res.status(201).json({ message: "Author added" });
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
+	
 };
 
-exports.getPostById = async (req, res, next) => {
-  try {
-    let postId = req.params.id;
+exports.findAuthorByID = async (req, res, next) => {
+	
+	try {
+		let authorID = req.params.id;
+		let [author, _] = await Author.findByID(authorID);
 
-    let [post, _] = await Post.findById(postId);
+		res.status(200).json({author});
+	} catch (error) {
+	
+	}
+};
 
-    res.status(200).json({ post: post[0] });
-  } catch (error) {
-    next(error);
-  }
+exports.findAllBooks = async (req, res, next) => {
+	//finds all books
+	try {
+		const [books, _] = await Books.findAllBooks();
+
+		res.status(200).json({ count: books.length, books});
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
+};
+
+exports.bookSalesDesc = async (req, res, next) => {
+	//finds all books and sorts them by sales desc by default
+	try {
+		const [books, _] = await Books.bookSalesDesc();
+
+		res.status(200).json({ count: books.length, books});
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
+};
+exports.bookSalesAsc = async (req, res, next) => {
+	//finds all books and sorts them by sales asc
+	try {
+		const [books, _] = await Books.bookSalesAsc();
+
+		res.status(200).json({ count: books.length, books});
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
+};
+exports.bookRatingAsc = async (req, res, next) => {
+	try {
+		const [books, _] = await Books.bookRatingAsc();
+
+		res.status(200).json({ count: books.length, books});
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
+};
+
+exports.bookRatingDesc = async (req, res, next) => {
+	try {
+		const [books, _] = await Books.bookRatingDesc();
+
+		res.status(200).json({ count: books.length, books});
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
 };
